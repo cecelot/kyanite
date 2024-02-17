3c6aeec8486ae3a75c778db9359b94defd2fdf55
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-02-17 09:54:31 -0600

    Convert builtins to "root" module

---
+++ crates/kyac/src/builtins.rs
@@ -0,0 +1,19 @@
+use crate::{ast::Ast, Source};
+
+const DEFINITIONS: &str = indoc::indoc! {"
+    extern fun println_str(s: str)
+    extern fun println_int(i: int)
+    extern fun println_float(f: float)
+    extern fun println_bool(b: bool)
+
+    extern fun max_int(a: int, b: int): int
+    extern fun min_int(a: int, b: int): int
+
+    extern fun max_float(a: float, b: float): float
+    extern fun min_float(a: float, b: float): float
+"};
+
+pub fn builtins() -> Ast {
+    let source = Source::in_memory(DEFINITIONS.to_string());
+    Ast::try_from(&source).unwrap()
+}
