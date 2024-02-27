dfe3aa4082d66d04f6fa546cf64aee27a845952d
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-02-27 15:18:08 -0600

    Link highlights crate to VSCode extension
    (minimally working sample with hardcoded highlights)

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,16 +1,43 @@
 import * as vscode from "vscode";
+import { highlights } from "@kyanite/highlights";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("kyanite-vscode: extension activated");
+  const semanticTokensProvider =
+    vscode.languages.registerDocumentSemanticTokensProvider(
+      selector,
+      provider,
+      legend
+    );
 
-  const disposable = vscode.commands.registerCommand(
-    "kyanite-vscode.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World from kyanite-vscode!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(semanticTokensProvider);
 }
+
+const tokenTypes = ["keyword"];
+const tokenModifiers: string[] = [];
+const legend = new vscode.SemanticTokensLegend(tokenTypes, tokenModifiers);
+
+const provider: vscode.DocumentSemanticTokensProvider = {
+  provideDocumentSemanticTokens(
+    document: vscode.TextDocument
+  ): vscode.ProviderResult<vscode.SemanticTokens> {
+    const tokensBuilder = new vscode.SemanticTokensBuilder(legend);
+    const tokens = highlights({
+      code: document.getText(),
+      filename: document.fileName,
+    });
+    for (const token of tokens) {
+      console.log(token);
+      const span = token.span;
+      const range = new vscode.Range(
+        new vscode.Position(span.line, span.start),
+        new vscode.Position(span.line, span.end)
+      );
+      tokensBuilder.push(range, token.kind, token.modifiers);
+    }
+    return tokensBuilder.build();
+  },
+};
+
+const selector = { language: "kyanite", scheme: "file" };
 
 export function deactivate() {}
