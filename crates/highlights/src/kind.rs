b7101ae242c7832fef94f11d34da3c90431538bc
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-02-28 09:56:14 -0600

    Remove unused Display impl

--- crates/highlights/src/kind.rs
+++ crates/highlights/src/kind.rs
@@ -1,14 +1,4 @@
-use std::fmt;
-
 #[napi(string_enum)]
 pub enum Kind {
     Keyword,
-}
-
-impl fmt::Display for Kind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Kind::Keyword => write!(f, "keyword"),
-        }
-    }
 }
